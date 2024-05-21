09870143d79c136d62b6cdfa2473c312a25e1e5e
Prajwal A K <prajwalhassan7@gmail.com>
2024-05-21 22:40:14 +0530

    UI Perfectly Setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from '@/components/Sidebar'
+import React from 'react'
+import PlaygroundPage from './playground/home'
+
+const page = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default page
